2f25f8bb224a8b8be08dad4391ecaec13e51149e
FRIASLE <FRIASLE@ZHA-ITA090-W7.corp.oocl.com>
2017-08-04 11:27:23 +0800

    add testFile.java

--- gitExample/src/main/java/gitExample/testFile.java
+++ gitExample/src/main/java/gitExample/testFile.java
@@ -4,6 +4,13 @@ public class testFile {
 	
 	public static void main(String args[]) {
 		System.out.println("Hello");
+		String myName = "Levi Frias";
+		String myEmail = "lf@oocl.com";
+		String myAddress = "PH";
+		
+		System.out.println(myName);
+		System.out.println(myEmail);
+		System.out.println(myAddress);
 	}
 
 }
