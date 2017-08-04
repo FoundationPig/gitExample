804b5ea5b56a884f916161a53dad4cb3cc5a25d5
FRIASLE <FRIASLE@ZHA-ITA090-W7.corp.oocl.com>
2017-08-04 11:45:02 +0800

    added zip conde in add zip code branch

--- gitExample/src/main/java/gitExample/testFile.java
+++ gitExample/src/main/java/gitExample/testFile.java
@@ -8,11 +8,14 @@ public class testFile {
 		String myEmail = "lf@oocl.com";
 		String myAddress = "PH";
 		String myGender = "M";
+		String zipCode = "165";
+		
 		
 		System.out.println(myName);
 		System.out.println(myEmail);
 		System.out.println(myAddress);
 		System.out.println(myGender);
+		System.out.println(zipCode);
 		
 		for(int x = 1;x<1;x++) {
 			
